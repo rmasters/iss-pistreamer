92040df31bb8c67d3f4a95ad233482150aea8c5b
Ross <ross@rossmasters.com>
2018-06-10 22:38:28 +0100

    Move the reload-on-fail to systemd

--- templates/iss.sh
+++ templates/iss.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-while true
-do
-    livestreamer "{{ iss_stream_url }}" "{{ iss_video_format }}" \
-        --player omxplayer \
-        --fifo \
-        --player-args "{% if iss_omx_resolution %}--win \"0 0 {{ iss_omx_resolution[0] }} {{ iss_omx_resolution[1] }}\" {% endif %}{filename}"
-done
+livestreamer "{{ iss_stream_url }}" "{{ iss_video_format }}" \
+    --player omxplayer \
+    --fifo \
+    --player-args "{% if iss_omx_resolution %}--win \"0 0 {{ iss_omx_resolution[0] }} {{ iss_omx_resolution[1] }}\" {% endif %}{filename}"
 
